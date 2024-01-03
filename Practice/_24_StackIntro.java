b8eb2845431efc68718c6ecbfc6a852f1ad556c0
= <arshsandhuprofessional@gmail.com>
2024-01-03 14:03:31 +0530

    03012024-02-StacksIntro

--- Practice/_24_StackIntro.java
+++ Practice/_24_StackIntro.java
@@ -1,5 +1,19 @@
 package Practice;
 
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+
 public class _24_StackIntro {
-    
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        while(!StdIn.isEmpty()){
+            stack.push(StdIn.readInt());
+        }
+
+        // Ctrl + Z -> Enter
+        
+        for(int i : stack){
+            System.out.println(i);
+        }
+    }
 }
