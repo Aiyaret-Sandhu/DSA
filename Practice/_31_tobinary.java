5d2347f637676a3028a809b5ce47a8877eb6b84a
= <arshsandhuprofessional@gmail.com>
2024-01-11 22:40:00 +0530

    11012024-04

--- Practice/_31_tobinary.java
+++ Practice/_31_tobinary.java
@@ -8,11 +8,20 @@ public class _31_tobinary {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
 
+        System.out.print("Enter the number : ");
         int N = StdIn.readInt();
 
         while (N > 0) {
-            
+            stack.push(N % 2);
+            N /= 2;
         }
+
+        System.out.print("The Binary representation is : ");
+
+        while(!(stack.isEmpty())) {
+            System.out.print(stack.pop());
+        }
+        System.out.println();
 
     }
 }
